562193a060035f9f268f8b682b5691c09fc1105f
yukihiro28 <yukihiro1228.2009@gmail.com>
2026-06-13 22:38:14 +0900

    Add Streamlit app for local event information and blog

---
+++ main.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.title("📅 地元イベント情報 ＆ ブログ")
+
+tab1, tab2 = st.tabs(["イベント情報", "ブログ"])
+
+with tab1:
+    st.header("今後のイベント予定")
+    st.subheader("📢 夏祭り 2026")
+    st.write("日時：2026年8月15日")
+    st.write("場所：中央公園")
+    if st.button("参加登録する"):
+        st.success("登録が完了しました！")
+
+with tab2:
+    st.header("管理人ブログ")
+    st.markdown("### PythonでWebサイトを作ってみた")
+    st.write("2026年6月13日 投稿")
+    st.write("iPadとPythonだけでサイトが作れました。最高。")
